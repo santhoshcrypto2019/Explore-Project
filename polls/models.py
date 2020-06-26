02a6eecddb9a0ae5cfcc392b21e17e71706cf339
santhosh kumar <santhosh.crypto2019@gmail.com>
2020-06-26 14:43:05 +0530

    part3 and part4

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
@@ -19,3 +21,6 @@ class Choice(models.Model):
     
     def __str__(self):
         return self.choice_text
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
